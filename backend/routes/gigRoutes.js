26575f98be31b1a18d326bc1790487e3ba4a2978
Muhammad-Ahmad-I <101197394+Muhammad-Ahmad-I@users.noreply.github.com>
2022-03-09 22:44:32 +0500

    Delete gigRoutes.js

--- backend/routes/gigRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { getAllGigs, addNewGig } = require('../controller/gigController.js');
-const route = express.Router();
-
-route.get('/', getAllGigs);
-route.get('/add', addNewGig);
-module.exports = route;
